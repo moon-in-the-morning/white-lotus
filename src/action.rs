09d1c92a3ee6472b5d1a6fa0a46263a69aab45a6
moon_in_the_morning <sm.goldstein2018@gmail.com>
2026-07-17 00:01:36 -0500

    action

---
+++ src/action.rs
@@ -0,0 +1,13 @@
+use crate::{NodeId, Payload};
+use crate::message::Message;
+
+pub enum Action<Id: NodeId, P: Payload> {
+//send a message to a specific peer
+	Send { to: Id, msg: Message<Id, P> },
+//open a live connection to a new peer
+	Connect { peer: Id },
+//tear down the connection to a peer we removed
+	Disconnect { peer: Id },
+//give recived payload to application
+	deliver { payload: P },
+}
